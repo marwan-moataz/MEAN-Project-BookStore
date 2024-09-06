0f36a6db0c565d5351b6090f79a8b20842019c7b
Marwan Moataz <159698698+marwan-moataz@users.noreply.github.com>
2024-09-06 16:09:44 +0300

    delete + unit test files comments

--- backend/Model/user.model.js
+++ backend/Model/user.model.js
@@ -1,67 +1,5 @@
-// import { Schema, model } from "mongoose";
-
-// export interface userBook {
-//   bookId: string;
-//   shelve: "read" | "want to read" | "reading" | null;
-//   rating: number;
-//   comment: string;
-// }
-// export interface User {
-//   id: string;
-//   email: string;
-//   password: any;
-//   confirmPassword: string;
-//   name: string;
-//   profilePicture: string;
-//   isAdmin: boolean;
-//   book: any;
-// }
-
-// export const UserSchema = new Schema(
-//   {
-//     name: { type: String, required: true },
-//     email: { type: String, Number, required: true, unique: true },
-//     password: { type: String, Number, required: true },
-//     confirmPassword: { type: String, required: true },
-//     isAdmin: { type: Boolean, required: true },
-//     profilePicture: { type: String, required: true },
-//   },
-//   {
-//     timestamps: true,
-//     toJSON: {
-//       virtuals: true,
-//     },
-//     toObject: {
-//       virtuals: true,
-//     },
-//   }
-// );
-
-// export const UserModel = model("User", UserSchema);
-
 const { Schema, model } = require("mongoose");
 
-// Define the userBook interface
-// export interface userBook {
-//   bookId: string;
-//   shelve: "read" | "want to read" | "reading" | null;
-//   rating: number;
-//   comment: string;
-// }
-
-// // Define the User interface
-// export interface User {
-//   id: string;
-//   email: string;
-//   password: any;
-//   confirmPassword: string;
-//   name: string;
-//   profilePicture: string;
-//   isAdmin: boolean;
-//   book: userBook[]; // Define the book field as an array of userBook objects
-// }
-
-// Define the schema for userBook
 const userBookSchema = new Schema({
   bookId: { type: String },
   shelve: {
@@ -71,7 +9,6 @@ const userBookSchema = new Schema({
   },
 });
 
-// Define the schema for User
 const UserSchema = new Schema(
   {
     name: { type: String, required: true },
@@ -80,7 +17,7 @@ const UserSchema = new Schema(
     confirmPassword: { type: String, required: true },
     isAdmin: { type: Boolean, required: true },
     profilePicture: { type: String, required: true },
-    book: { type: [userBookSchema], default: [] }, // Add the book field using the userBook schema
+    book: { type: [userBookSchema], default: [] },
   },
   {
     timestamps: true,
@@ -93,7 +30,6 @@ const UserSchema = new Schema(
   }
 );
 
-// Create and export the UserModel
 const UserModel = model("User", UserSchema);
 
 module.exports = { UserModel };
